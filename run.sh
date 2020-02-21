306fd7a53412c0c5a9fe766ee6bcae7abf67982a
Soroush Javadi <soroush.javadi@gmail.com>
2020-02-21 11:18:07 +0330

    Remove --build
    No longer using a Dockerfile.

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ DB_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/db" >/dev/null 2>&1 && pwd )"
 cd "$DB_DIR"
 
 echo '* Initializing db...'
-docker-compose up -d --build
+docker-compose up -d
 
 echo '* Waiting for db to start...'
 until psql -U postgres -h localhost -p 5533 -c 'SELECT 1' >/dev/null 2>&1; do
